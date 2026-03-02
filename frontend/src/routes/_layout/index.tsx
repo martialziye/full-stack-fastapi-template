00dd49ed2ad7965bc6b8043deb74b81aabd6ae96
Ziye WANG <ziyewang438@gmail.com>
2026-03-02 20:35:51 +0100

    new scroll invisible scrollbar

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -31,9 +31,6 @@ function Dashboard() {
         <h1 className="text-2xl truncate max-w-sm">
           Hi, {currentUser?.full_name || currentUser?.email}
         </h1>
-        <p className="text-muted-foreground">
-          Welcome back, nice to see you again!!!
-        </p>
       </div>
 
       <section className="rounded-lg border bg-card p-4">
